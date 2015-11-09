969320d3e39d97868561db06c52496013df62560
Ivan Arkhipov <vanchez666@gmail.com>
2015-11-10 01:10:25 +0300

    корзина теперь тоже всплывает, весь js объединил в один файл

--- js/main.js
+++ js/main.js
@@ -62,11 +62,20 @@ var basketItem = document.querySelectorAll(".item");
 var basketPopup = document.querySelector(".modal-basket");
 var basketClose = basketPopup.querySelector(".close");
 var basketCont = basketPopup.querySelector(".btn_white");
-      
-// пока не получается сделать выбор сразу нескольких кнопок      
-      
-     
-basketClose.addEventListener("click", function(event) {
+
+function openBasket(event) {
+    event.preventDefault();
+    basketPopup.classList.add("modal-content-show");    
+}
+
+for(var i = 0; i < basketItem.length; i++) {
+  var basketLink = basketItem[i].querySelector(".buy");
+  basketLink.addEventListener("click", openBasket);
+}
+
+// окно стало всплывать, но не уверен, что верно сделал
+
+basketClose.addEventListener("click", function (event) {
   event.preventDefault();
   basketPopup.classList.remove("modal-content-show");
 });
@@ -79,7 +88,7 @@ basketCont.addEventListener("click", function(event) {
 window.addEventListener("keydown", function(event) {
   if (event.keyCode === 27) { 
     if (basketPopup.classList.contains("modal-content-show")) {
-      basketPopup.classList.remove("modal-content-show");
+       basketPopup.classList.remove("modal-content-show");
     }
   } 
 });
--- js/modal-basket.js
+++ js/modal-basket.js
@@ -1,25 +1,35 @@
-      var basketItem = document.querySelectorAll(".item");                
-      var basketPopup = document.querySelector(".modal-basket");
-      var basketClose = basketPopup.querySelector(".close");
-      var basketCont = basketPopup.querySelector(".btn_white");
+var basketItem = document.querySelectorAll(".item");                
+var basketPopup = document.querySelector(".modal-basket");
+var basketClose = basketPopup.querySelector(".close");
+var basketCont = basketPopup.querySelector(".btn_white");
+
+function openBasket(event) {
+    event.preventDefault();
+    basketPopup.classList.add("modal-content-show");    
+}
+
+for(var i = 0; i < basketItem.length; i++) {
+  var basketLink = basketItem[i].querySelector(".buy");
+  basketLink.addEventListener("click", openBasket);
+}
+
+// окно стало всплывать, но не уверен, что верно сделал
+
+basketClose.addEventListener("click", function (event) {
+  event.preventDefault();
+  basketPopup.classList.remove("modal-content-show");
+});
       
-      // пока не получается сделать выбор сразу нескольких кнопок      
+basketCont.addEventListener("click", function(event) {
+  event.preventDefault();
+  basketPopup.classList.remove("modal-content-show");
+});
       
-      
-      basketClose.addEventListener("click", function(event) {
-        event.preventDefault();
-        basketPopup.classList.remove("modal-content-show");
-      });
-      
-      basketCont.addEventListener("click", function(event) {
-        event.preventDefault();
-        basketPopup.classList.remove("modal-content-show");
-      });
-      
-      window.addEventListener("keydown", function(event) {
-        if (event.keyCode === 27) { 
-          if (basketPopup.classList.contains("modal-content-show")) {
-            basketPopup.classList.remove("modal-content-show");
-          }
-        } 
-      });
+window.addEventListener("keydown", function(event) {
+  if (event.keyCode === 27) { 
+    if (basketPopup.classList.contains("modal-content-show")) {
+       basketPopup.classList.remove("modal-content-show");
+    }
+  } 
+});
+
