ce958db4b05672034ef7f82260d64cfc559aa219
Ivan Arkhipov <vanchez666@gmail.com>
2015-11-10 01:50:55 +0300

    добавил уменьшенные стили и js

--- js/main.js
+++ js/main.js
@@ -1,61 +1,3 @@
-// форма обратной связи
-
-var writeusLink = document.querySelector(".btn-writeus");
-var writeusPopup = document.querySelector(".modal-writeus");
-var writeusClose = writeusPopup.querySelector(".close");
-var writeusLogin = writeusPopup.querySelector("[name='name']");
-var writeusCancel = writeusPopup.querySelector("[type='reset']");
-      
-writeusLink.addEventListener("click", function(event) {
-  event.preventDefault();
-  writeusPopup.classList.add("modal-content-show");
-  writeusLogin.focus();
- });
-      
-writeusClose.addEventListener("click", function(event) {
-  event.preventDefault();
-  writeusPopup.classList.remove("modal-content-show");                
-});
-
-writeusCancel.addEventListener("click", function(event) {
-  event.preventDefault();
-  writeusPopup.classList.remove("modal-content-show");  
-});    
-      
-window.addEventListener("keydown", function(event) {
-  if (event.keyCode === 27) {
-    if (writeusPopup.classList.contains("modal-content-show")) {
-      writeusPopup.classList.remove("modal-content-show");                          
-    }
-  }
-});
-
-
-// карта 
-
-var mapLink = document.querySelector(".open-map");
-var mapPopup = document.querySelector(".modal-map");
-var mapClose = mapPopup.querySelector(".close");
-      
-mapLink.addEventListener("click", function(event) {
-  event.preventDefault();
-  mapPopup.classList.add("modal-content-show");
-});
-      
-mapClose.addEventListener("click", function(event) {
-  event.preventDefault();
-  mapPopup.classList.remove("modal-content-show");                
-});
-      
-window.addEventListener("keydown", function(event) {
-  if (event.keyCode === 27) {
-    if (mapPopup.classList.contains("modal-content-show")) {
-      mapPopup.classList.remove("modal-content-show");                          
-    }
-  }
-});
-
-
 // корзина
 
 var basketItem = document.querySelectorAll(".item");                
@@ -91,4 +33,67 @@ window.addEventListener("keydown", function(event) {
        basketPopup.classList.remove("modal-content-show");
     }
   } 
+});
+
+
+
+
+// форма обратной связи
+
+var writeusLink = document.querySelector(".btn-writeus");
+var writeusPopup = document.querySelector(".modal-writeus");
+var writeusClose = writeusPopup.querySelector(".close");
+var writeusLogin = writeusPopup.querySelector("[name='name']");
+var writeusCancel = writeusPopup.querySelector("[type='reset']");
+      
+writeusLink.addEventListener("click", function(event) {
+  event.preventDefault();
+  writeusPopup.classList.add("modal-content-show");
+  writeusLogin.focus();
+ });
+      
+writeusClose.addEventListener("click", function(event) {
+  event.preventDefault();
+  writeusPopup.classList.remove("modal-content-show");                
+});
+
+writeusCancel.addEventListener("click", function(event) {
+  event.preventDefault();
+  writeusPopup.classList.remove("modal-content-show");  
+});    
+      
+window.addEventListener("keydown", function(event) {
+  if (event.keyCode === 27) {
+    if (writeusPopup.classList.contains("modal-content-show")) {
+      writeusPopup.classList.remove("modal-content-show");                          
+    }
+  }
+});
+
+
+
+
+
+// карта 
+
+var mapLink = document.querySelector(".open-map");
+var mapPopup = document.querySelector(".modal-map");
+var mapClose = mapPopup.querySelector(".close");
+      
+mapLink.addEventListener("click", function(event) {
+  event.preventDefault();
+  mapPopup.classList.add("modal-content-show");
+});
+      
+mapClose.addEventListener("click", function(event) {
+  event.preventDefault();
+  mapPopup.classList.remove("modal-content-show");                
+});
+      
+window.addEventListener("keydown", function(event) {
+  if (event.keyCode === 27) {
+    if (mapPopup.classList.contains("modal-content-show")) {
+      mapPopup.classList.remove("modal-content-show");                          
+    }
+  }
 });
